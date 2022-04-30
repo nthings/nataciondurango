4151c4f613ed0c33ab90d1c83172578789368da5
Mauricio Alejandro Martínez Pacheco <n_othing@hotmail.com>
2022-04-30 14:26:47 -0500

    fix

--- src/routes/upload.router.ts
+++ src/routes/upload.router.ts
@@ -3,22 +3,9 @@ import UploadController from "../controllers/upload.controller";
 
 const router = express.Router();
 
-router.get("/", async (_req, res) => {
-  const controller = new UploadController();
-  const response = await controller.getUploads();
-  return res.send(response);
-});
-
 router.post("/", async (req, res) => {
   const controller = new UploadController();
   const response = await controller.createUpload(req.body);
-  return res.send(response);
-});
-
-router.get("/:id", async (req, res) => {
-  const controller = new UploadController();
-  const response = await controller.getUpload(req.params.id);
-  if (!response) res.status(404).send({message: "No user found"})
   return res.send(response);
 });
 
